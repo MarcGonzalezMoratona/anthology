11afd7a6565fb1ed6caaf7dbcde5e7d35c9dc244
Marc González Moratona <80526953+MarcGonzalezMoratona@users.noreply.github.com>
2024-09-26 09:10:55 +0200

    course finished

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,86 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './button';
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  args: {
+    children: 'Button',
+    variant: 'primary',
+    size: 'medium',
+    disabled: false,
+  },
+  argTypes: {
+    children: {
+      name: 'Label',
+      control: 'text',
+      description: 'The text displayed on the button.',
+      table: {
+        disable: true,
+      },
+    },
+    size: { control: { type: 'select' } },
+    disabled: { control: 'boolean' },
+    variant: {
+      control: {
+        type: 'select',
+      },
+    },
+  },
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Secondary',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+  },
+};
+
+export const Dark: Story = {
+  parameters: {
+    themes: {
+      themeOverride: 'dark',
+    },
+  },
+};
+
+export const Mobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: 'mobile1',
+    },
+  },
+};
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,17 @@
-export const Button = (props: any) => {
-  return <button {...props} />;
+import { ComponentProps } from 'react';
+
+import { variants, type ButtonVariants } from './button-variant';
+
+export type ButtonProps = ComponentProps<'button'> &
+  ButtonVariants & {
+    size?: 'small' | 'medium' | 'large';
+  };
+
+export const Button = ({
+  variant = 'primary',
+  size = 'medium',
+  className,
+  ...props
+}: ButtonProps) => {
+  return <button {...props} className={variants({ variant, size })} />;
 };
